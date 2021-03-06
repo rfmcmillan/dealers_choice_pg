ca82595d55e32db1fb9764e00226b538bd92c710
Russel McMillan <71411627+rfmcmillan@users.noreply.github.com>
2021-03-06 08:12:52 -0500

    added POST route

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express();
 const { syncAndSeed, setUp, client } = require('./db');
 const path = require('path');
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'assets')));
 
 app.get('/', async (req, res, next) => {
@@ -22,11 +23,46 @@ app.get('/', async (req, res, next) => {
           })
           .join('')}
         </ul>
+        <h3>Add A Book</h3>
+        <form method="POST" action="/">
+          <label>ID*:</label>
+          <input name="ID" type="number"></input>
+          <br>
+          <label>Title*:</label>
+          <input name="TITLE"></input>
+          <br>
+          <label>Author*:</label>
+          <input name="AUTHOR"></input>
+          <br>
+          <label>Year:</label>
+          <input name="YEAR"></input>
+          <br>
+          <label>Genre:</label>
+          <input name="GENRE"></input>
+          <br>
+          <button>Create Book</button>
+        </form>
       </div>
     </body>
   </html>
   `;
     res.send(html);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post('/', async (req, res, next) => {
+  try {
+    console.log('req.body:', req.body);
+    const newBook = req.body;
+    console.log('newBook:', newBook);
+    const SQL = `
+    INSERT INTO books (ID, TITLE, AUTHOR, YEAR, GENRE) VALUES (${newBook.ID}, '${newBook.TITLE}', '${newBook.AUTHOR}', ${newBook.YEAR}, '${newBook.GENRE}');
+    `;
+    console.log('SQL:', SQL);
+    await client.query(SQL);
+    res.redirect('/');
   } catch (error) {
     next(error);
   }
